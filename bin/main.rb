c6859e17feda3a3fbb421ac599835f3c0cf2ee0f
vicmaburrito <65370617+vicmaburrito@users.noreply.github.com>
2021-02-16 09:35:34 -0600

    Adding required changes

--- bin/main.rb
+++ bin/main.rb
@@ -15,17 +15,22 @@ puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
 \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
 
 puts "#{Player1} it is your turn, please make your move"
+puts 'Move is not valid, Make a valid move'
 Player1Move = gets.chomp
-game_status[Player1Move.to_i - 1] = 'X'
+game_status[Player1Move.to_i - 1]
 
 puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
 \n#{game_status[3]} | #{game_status[4]} | #{game_status[5]}\n ---------
 \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
 
 puts "#{Player2} it is your turn, please make your move"
+puts 'Move is not valid, Make a valid move'
 Player2Move = gets.chomp
-game_status[Player2Move.to_i - 1] = 'O'
+game_status[Player2Move.to_i - 1]
 
 puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
 \n#{game_status[3]} | #{game_status[4]} | #{game_status[5]}\n ---------
 \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
+
+puts "#{Player1} its the winner!"
+puts 'The game is a draw'
