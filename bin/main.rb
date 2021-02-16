d90a69653d5b80a5a7460c96799b35d4a16a10c6
vicmaburrito <65370617+vicmaburrito@users.noreply.github.com>
2021-02-16 16:57:10 -0600

    Added requires changes

--- bin/main.rb
+++ bin/main.rb
@@ -15,8 +15,12 @@ puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
 \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
 
 puts "#{Player1} it is your turn, please make your move"
-puts 'Move is not valid, Make a valid move'
 Player1Move = gets.chomp
+if Player1Move.include? '10'
+  puts 'Move is not valid, you loose your turn'
+else
+  puts "Good! #{Player1}"
+end
 game_status[Player1Move.to_i - 1]
 
 puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
@@ -24,13 +28,20 @@ puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
 \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
 
 puts "#{Player2} it is your turn, please make your move"
-puts 'Move is not valid, Make a valid move'
 Player2Move = gets.chomp
+if Player2Move.include? '10'
+  puts 'Move is not valid, you loose your turn'
+else
+  puts "Good! #{Player2}"
+end
 game_status[Player2Move.to_i - 1]
 
 puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
 \n#{game_status[3]} | #{game_status[4]} | #{game_status[5]}\n ---------
 \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
 
-puts "#{Player1} its the winner!"
-puts 'The game is a draw'
+if Player1Move == Player2Move
+  puts 'its a draw'
+else
+  puts "#{Player1} it's a winner"
+end
