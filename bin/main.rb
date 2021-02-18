be5a46a3b33bb7948962f807ac5a0289496537f4
vicmaburrito <65370617+vicmaburrito@users.noreply.github.com>
2021-02-18 14:26:05 -0600

    Fixing Changes Required

--- bin/main.rb
+++ bin/main.rb
@@ -9,39 +9,39 @@ puts 'Player 2 enter your name'
 Player2 = gets.chomp
 
 game_status = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+wins = false
+start = 2
 
-puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
+while start < 11
+
+  puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
 \n#{game_status[3]} | #{game_status[4]} | #{game_status[5]}\n ---------
 \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
 
-puts "#{Player1} it is your turn, please make your move"
-Player1Move = gets.chomp
-if Player1Move.include? '10'
-  puts 'Move is not valid, you loose your turn'
-else
-  puts "Good! #{Player1}"
+  if start.even?
+    current_player = Player1
+    puts "#{current_player} it is your turn, please make your move"
+    player_1_turn = gets.chomp.to_i
+  else
+    current_player = Player2
+    puts "#{current_player} it is your turn, please make your move"
+    player_2_turn = gets.chomp.to_i
+  end
+
+  puts 'Move is not valid' if player_1_turn < 1 && player_1_turn > 9
+
+  puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
+  \n#{game_status[3]} | #{game_status[4]} | #{game_status[5]}\n ---------
+  \n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
+  if wins
+    puts "#{current_player} is the winner!"
+    break
+  end
+
+  start += 1
+
 end
-game_status[Player1Move.to_i - 1]
 
-puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
-\n#{game_status[3]} | #{game_status[4]} | #{game_status[5]}\n ---------
-\n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
-
-puts "#{Player2} it is your turn, please make your move"
-Player2Move = gets.chomp
-if Player2Move.include? '10'
-  puts 'Move is not valid, you loose your turn'
-else
-  puts "Good! #{Player2}"
-end
-game_status[Player2Move.to_i - 1]
-
-puts "#{game_status[0]} | #{game_status[1]} | #{game_status[2]} \n ---------
-\n#{game_status[3]} | #{game_status[4]} | #{game_status[5]}\n ---------
-\n#{game_status[6]} | #{game_status[7]} | #{game_status[8]}"
-
-if Player1Move == Player2Move
-  puts 'its a draw'
-else
-  puts "#{Player1} it's a winner"
-end
+puts "there's a draw" if wins == false
+puts player_1_turn
+puts player_2_turn
