a894e981c5522ffeeaa578faf6ec46a12e8605d9
Godwin Nwachukwu <nwgwin@gmail.com>
2021-03-29 20:58:32 +0100

    test pass for self.squares method in Board class passed

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,1 +1,38 @@
 require './lib/tic_tac_toe.rb'
+
+RSpec.describe Board do
+  describe '#self.squares' do
+    context 'when game is started,' do
+      it 'returns an object of board slots' do
+        board_cells = Board.squares
+        slots = {
+          1 => '-',
+          2 => '-',
+          3 => '-',
+          4 => '-',
+          5 => '-',
+          6 => '-',
+          7 => '-',
+          8 => '-',
+          9 => '-'
+        }
+        expect(board_cells).to eql(slots)
+      end
+    end
+  end
+end
+
+describe Player do
+end
+
+describe Game do
+end
+
+describe Scoreboard do
+end
+
+describe AutoPlayer do
+end
+
+describe AutoGame do
+end
