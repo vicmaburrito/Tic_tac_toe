2171d218e6a8b03736c760622c61f06b46b75d97
vicmaburrito <vmaldaraca@gmail.com>
2021-04-15 11:08:58 -0500

    adding scoreboard

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -200,7 +200,35 @@ RSpec.describe Game do
   end
 end
 
-describe Scoreboard do
+RSpec.describe Scoreboard do
+  describe '#increment_score' do
+    context 'when there is a winner' do
+      it 'increases the players score by one' do
+        win_letter = 'X'
+        subject.increment_score(win_letter)
+        expect(subject.x_score).to eql(1)
+      end
+    end
+    context 'when there is a winner' do
+      it 'increases the players score by one' do
+        win_letter = 'O'
+        subject.increment_score(win_letter)
+        expect(subject.o_score).to eql(1)
+      end
+    end
+    context 'when there is no winner' do
+      it 'increases the players score by zero' do
+        subject.increment_score(win_letter = nil)
+        expect(subject.x_score).to eql(0)
+      end
+    end
+    context 'when there is a winner' do
+      it 'increases the players score by zero' do
+        subject.increment_score(win_letter = nil)
+        expect(subject.o_score).to eql(0)
+      end
+    end
+  end
 end
 
 describe AutoPlayer do
